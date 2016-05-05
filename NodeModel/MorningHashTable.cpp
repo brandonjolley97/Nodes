cf62c5c3a759966bf05d870c3aa17779b435a634
brandonjolley97 <bjol7457@740s-pl-132.local>
2016-05-05 09:44:42 -0600

    Added a hashTable header and source file.  Added basic methods to get started

---
+++ NodeModel/MorningHashTable.cpp
@@ -0,0 +1,53 @@
+
+//  MorningHashTable.cpp
+//  Nodes
+//
+//  Created by Jolley, Brandon on 5/5/16.
+//  Copyright © 2016 Jolley, Brandon. All rights reserved.
+//
+
+#include "MorningHashTable.hpp"
+
+template <class Type>
+MorningHashTable<Type> :: MorningHashTable()
+{
+    this->capacity = 101;
+    this->efficiencyPercentage = .667;
+    this->size = 0;
+    this->internalStorage = new Type[capacity];
+    
+}
+
+template <class Type>
+MorningHashTable<Type> :: ~MorningHashTable<Type>()
+{
+    delete [] internalStorage;
+}
+
+template <class Type>
+int MorningHashTable<Type> :: getSize()
+{
+    return this->size();
+}
+
+template<class Type>
+void MorningHashTable<Type> :: add(const Type& value)
+{
+    int positionToInsert = findPostion(value);
+    
+    if(internalStorage[positionToInsert] != nullptr)
+    {
+        //Loop over the internalStorage to find the next empty slot.
+        //Insert the value there.
+        while(internalStorage[positionToInsert] != nullptr)
+        {
+            positionToInsert = positionToInsert +1 % size;
+        }
+        internalStorage[positionToInsert] = value;
+        
+    }
+    else
+    {
+        internalStorage[positionToInsert] = value;
+    }
+}
---
+++ NodeModel/MorningHashTable.hpp
@@ -0,0 +1,36 @@
+//
+//  MorningHashTable.hpp
+//  Nodes
+//
+//  Created by Jolley, Brandon on 5/5/16.
+//  Copyright © 2016 Jolley, Brandon. All rights reserved.
+//
+
+#ifndef MorningHashTable_hpp
+#define MorningHashTable_hpp
+
+#include <stdio.h>
+template<class Type>
+class MorningHashTable
+{
+private:
+    int capacity;
+    double efficiencyPercentage;
+    int size;
+    Type * internalStorage;
+    
+    int findPosition(const Type& value);
+    int handleCollision(const Type& value);
+    void updateSize();
+public:
+    MorningHashTable();
+    ~MorningHashTable();
+    
+    void add(const Type& value);
+    bool remove(const Type& value);
+    bool contains(const Type& value);
+    int getSize();
+};
+
+#endif /* MorningHashTable_hpp */
+
